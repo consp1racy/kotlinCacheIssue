81acc76be5864222e0320e4d48bb73964b75f9f7
Eugen Pechanec <e.pechanec@gmail.com>
2019-06-01 17:25:49 +0200

    Gradle 5.4.1, AGP 3.4.1, Kotlin 1.3.31, Dagger 2.23.1, trimming
    Kotlin source only.
    Disabled R.class generation.
    Disabled release variant so you can run "gradlew assemble" to test.
    Removed AppCompat, app icon, custom theme and empty layout.

--- app/src/main/java/com/mykotlindaggerapplication/MainActivity.kt
+++ app/src/main/java/com/mykotlindaggerapplication/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.mykotlindaggerapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import com.mykotlindaggerapplication.dagger.DaggerActivityComponent
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     @Inject lateinit var classToConvert: ClassToConvert
 
     override fun onCreate(savedInstanceState: Bundle?) {
         DaggerActivityComponent.builder().build().injectMainActivity(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
